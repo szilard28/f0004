1fd01ee879e9310931110807b76949d68d0f776e
szilard28 <71643751+szilard28@users.noreply.github.com>
2020-11-02 14:47:12 +0100

    Update f0004a.py

--- f0004a.py
+++ f0004a.py
@@ -3,5 +3,3 @@ kor = input('És hány éves kend? ')
 kor = int(kor) 
 születési_év = 2020 - kor
 print(név, ', kend ', születési_év, '-ban született.', sep='')
-érettségi_év = születési_év + 18
-print(név, ', kend ', érettségi_év, '-ban érettségizik.', sep='')
