2fd85128f42cd60000394ddf9970ec595bf6d35d
szilard28 <wmtech.jandoszilard@gmail.com>
2020-11-02 13:21:24 +0000

    f0004b.py

--- f0004a.py
+++ f0004a.py
@@ -1,5 +1,7 @@
-név = input('Mikor született? ')
+név = input('Hogy híjják kendet? ')
 kor = input('És hány éves kend? ')
-kor = int(kor)
+kor = int(kor) 
 születési_év = 2020 - kor
 print(név, ', kend ', születési_év, '-ban született.', sep='')
+érettségi_év = születési_év + 18
+print(név, ', kend ', érettségi_év, '-ban érettségizik.', sep='')
